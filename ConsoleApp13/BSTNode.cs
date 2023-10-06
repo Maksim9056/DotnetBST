7314b4e975d12b23e131aa3283aa9a0efd3835e2
Maksim <bobreczovm@inbox.ru>
2023-10-06 11:36:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/BSTNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class BSTNode
+    {
+        public int key; public BSTNode left; public BSTNode right; public BSTNode(int key) { this.key = key; this.left = null; this.right = null; }
+    }
+    class BST 
+    { public BSTNode root;
+        public void Insert(int key) 
+        { root = InsertHelper(root, key); }
+        private BSTNode InsertHelper(BSTNode node, int key)
+        { if (node == null) { return new BSTNode(key); } if (key < node.key) { node.left = InsertHelper(node.left, key); } else if (key > node.key) { node.right = InsertHelper(node.right, key); } return node; } public void Visualize() { VisualizeHelper(root, "", true); } private void VisualizeHelper(BSTNode node, string prefix, bool isLeft) { if (node == null) { return; } string nodeStr = node.key.ToString(); string line = prefix + (isLeft ? "├── " : "└── ");
+            Console.WriteLine(line + nodeStr);
+            string childPrefix = prefix + (isLeft ? "│ " : " "); VisualizeHelper(node.left, childPrefix, true);
+            VisualizeHelper(node.right, childPrefix, false); } public void InOrderTraversal() { InOrderTraversalHelper(root);
+            Console.WriteLine(); } private void InOrderTraversalHelper(BSTNode node) { if (node != null) { InOrderTraversalHelper(node.left); 
+                Console.Write(node.key + " "); InOrderTraversalHelper(node.right); } } public void PreOrderTraversal() { PreOrderTraversalHelper(root); Console.WriteLine(); } private void PreOrderTraversalHelper(BSTNode node) { if (node != null) { Console.Write(node.key + " "); PreOrderTraversalHelper(node.left); 
+                PreOrderTraversalHelper(node.right); } } }
+}
+
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            BST bst = new BST(); 
+            // Insert nodes into the
+            int[] anArrayNodes = { 17, 6, 5, 20, 19, 18, 11, 14, 12, 13, 2, 4, 10 };
+            foreach (int node in anArrayNodes) { bst.Insert(node); } 
+            // Visualize the BST
+            bst.Visualize();
+            // In-order traversal of BST
+            Console.Write("In-order Traversal: "); bst.InOrderTraversal();
+            // Pre-order traversal of BST
+           Console.Write("Pre-order Traversal: "); bst.PreOrderTraversal(); 
+        
+          Console.ReadLine();
+        
+        }
+
+
+
+       
+    }
+        
+    
+}
